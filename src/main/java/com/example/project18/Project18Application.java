0beb044c9930e0673aa6444461277b892809c823
gbangsu <gu827@outlook.com>
2018-08-22 16:16:16 -0400

    Initial commit

---
+++ src/main/java/com/example/project18/Project18Application.java
@@ -0,0 +1,12 @@
+package com.example.project18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project18Application.class, args);
+	}
+}
